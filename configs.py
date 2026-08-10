4a4905ae6693122f4f7c8870ed43fdace8c1e999
Kim Jimin <kimjimin1507@gmail.com>
2026-08-10 13:43:03 +0800

    Update and rename configs/data_params.py to configs.py

--- configs.py
+++ configs.py
@@ -13,3 +13,14 @@ class DataParameters:
     test_split:float = 0.3
 
     inference:bool = False
+
+
+@dataclass
+class ModelConfig:
+    d_model:int = 8
+    dim_ffn:int = 16
+    max_window:int = 5
+    num_heads:int = 2
+    num_layers:int = 3
+    num_vars:int = 3
+    dropout:float = 0.2
