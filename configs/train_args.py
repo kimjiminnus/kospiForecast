82cf0bf1f8da22ee91dbc3360ee09f4a4b7f4d7b
Kim Jimin <kimjimin1507@gmail.com>
2026-08-10 13:43:13 +0800

    Update train_args.py

--- configs/train_args.py
+++ configs/train_args.py
@@ -1,10 +1,1 @@
-#DEFAULT ARGS
 
-@dataclass
-class TrainingArguments:
-    epochs: int = 50
-    train_batch_size: int = 8
-    eval_batch_size: int = 8
-    learning_rate: float = 1e-2
-    weight_decay: float = 1e-4
-    criterion :str = 'mse'
