a795455db7f1113a126f0f21179ad192261a7108
Kim Jimin <kimjimin1507@gmail.com>
2026-08-15 13:48:59 +0800

    remove LSTM instantiation option

--- models/create_model.py
+++ models/create_model.py
@@ -5,8 +5,6 @@ def create_model(model_type:str, model_config: ModelConfig):
 
     elif model_type.upper() == "TIMESFM":
         return ZeroShotTimesFM()
-
-    elif model_type.upper() == "LSTM":
-        return LSTM(model_config)
+        
     else:
         raise ValueError("Unknown model type")
