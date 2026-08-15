b0c34a4ab193d113264c0b0a9de774fead65b97f
Kim Jimin <kimjimin1507@gmail.com>
2026-08-15 14:39:05 +0800

    Update create_model.py

--- models/create_model.py
+++ models/create_model.py
@@ -1,3 +1,8 @@
+from src.configs import ModelConfig
+from src.models.timesfm import ZeroShotTimesFM
+from src.models.custom_decoder import Decoder
+
+
 def create_model(model_type:str, model_config: ModelConfig):
 
     if model_type.upper() == "DECODER":
